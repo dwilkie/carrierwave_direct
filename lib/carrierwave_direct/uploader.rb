3d33860d504e8a0f221001598b7012c710fed6d3
Tyler Love <t@tylr.org>
2011-12-06 01:44:24 -0500

    Update lib/carrierwave_direct/uploader.rb

--- lib/carrierwave_direct/uploader.rb
+++ lib/carrierwave_direct/uploader.rb
@@ -27,7 +27,7 @@ module CarrierWaveDirect
           Rails.logger.debug(key)
           uri = URI.parse(fog_uri)
           path = "/#{key}"
-          uri.path = path
+          uri.path = URI.escape(path)
           fog_uri = uri.to_s
         end
         fog_uri
