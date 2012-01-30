1300573a102a725e7f3665ba5c7bc2f3ec34d457
Chris Blunt <chris@plymouthsoftware.com>
2012-01-30 12:34:11 +0000

    Ensure specs load ViewHelpers before FormBuilderHelpers (dependency)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,8 @@
 require 'carrierwave_direct'
 require 'json'
 require 'timecop'
+
+require File.dirname(__FILE__) << '/support/view_helpers' # Catch dependency order
+
 Dir[ File.dirname(__FILE__) << "/support/**/*.rb"].each {|file| require file }
 
