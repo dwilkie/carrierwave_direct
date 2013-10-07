3fe93bd919446b08366a479ff6bae0dc87bf721f
Nick DeLuca <nick@wescrimmage.com>
2013-10-07 15:36:21 -0500

    Refactored mount spec to use new rspec syntax and slight clean up

--- spec/mount_spec.rb
+++ spec/mount_spec.rb
@@ -17,43 +17,30 @@ describe CarrierWaveDirect::Mount do
     it_should_have_accessor(:remote_video_net_url)
 
     describe "#has_video_upload?" do
-      context "video does not have a key" do
-        before { subject.video.stub(:has_key?).and_return(false) }
-
-        it "should return false" do
-          subject.should_not have_video_upload
-        end
+      it "returns false when video does not have a key" do
+        allow(subject.video).to receive(:has_key?).and_return(false)
+        expect(subject).to_not have_video_upload
       end
 
-      context "video has a key" do
-        before { subject.video.stub(:has_key?).and_return(true) }
-
-        it "should return true" do
-          subject.should have_video_upload
-        end
+      it "returns true when video has a key" do
+        allow(subject.video).to receive(:has_key?).and_return(true)
+        expect(subject).to have_video_upload
       end
     end
 
     describe "#has_remote_video_net_url?" do
-      context "remote_video_net_url is nil" do
-        before { subject.remote_video_net_url = nil }
-
-        it "should return false" do
-          subject.should_not have_remote_video_net_url
-        end
+      it "returns false when remote_video_net_url is nil" do
+        subject.remote_video_net_url = nil
+        expect(subject).to_not have_remote_video_net_url
       end
 
-      context "remote_video_net_url is not nil" do
-        before { subject.remote_video_net_url = "something" }
-
-        it "should return true" do
-          subject.should have_remote_video_net_url
-        end
+      it "returns true when remote_video_net_url is not nil" do
+        subject.remote_video_net_url = :not_nil
+        expect(subject).to have_remote_video_net_url
       end
     end
 
-    it_should_delegate(:key, :to => "video#key", :accessible => {"has_video_upload?" => false})
-
+    it_should_delegate(:key, :to => "video#key", :accessible => { "has_video_upload?" => false })
   end
 end
 
