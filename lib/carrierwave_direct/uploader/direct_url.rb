54eeb4434e99d452b939f20adfa4f65723671f03
Petrik <petrik@deheus.net>
2014-09-08 17:27:19 +0200

    Use the Carrierwave generated url for the direct_fog_url(:with_path => true) The current implmentation has issues while Carrierwave already has a working solution. This also make direct_fog_url work with private storage on S3

--- lib/carrierwave_direct/uploader/direct_url.rb
+++ lib/carrierwave_direct/uploader/direct_url.rb
@@ -3,14 +3,11 @@ module CarrierWaveDirect
     module DirectUrl
 
       def direct_fog_url(options = {})
-        fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), nil).public_url
         if options[:with_path]
-          uri = URI.parse(fog_uri.chomp('/'))
-          path = "/#{URI.decode(key)}"
-          uri.path += URI.escape(path)
-          fog_uri = uri.to_s
+          url
+        else
+          CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), nil).public_url
         end
-        fog_uri
       end
 
     end
