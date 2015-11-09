a625691ed0d95c724f2d0dbd48dbe214c1ee5bca
Javier <javier.siado@koombea.com>
2015-11-09 17:45:33 -0500

    adding deprecation warnings

--- lib/carrierwave_direct/mount.rb
+++ lib/carrierwave_direct/mount.rb
@@ -23,6 +23,14 @@ module CarrierWaveDirect
       include mod
       mod.class_eval <<-RUBY, __FILE__, __LINE__+1
 
+        def key
+          warn "key method is deprecated, please use column_key method instead."
+        end
+
+        def key=(k)
+          warn "key= method is deprecated, please use column_key= method instead."
+        end
+
         def #{column}_key
           send(:#{column}).key
         end
