21706cce6485f66266af4f66b7a37fdeeaf07341
MatthiasEngh <matthias@wescrimmage.com>
2017-05-30 11:26:27 -0500

    adjusting config to fit carrierwave-aws config rather than fog

--- spec/support/carrier_wave_config.rb
+++ spec/support/carrier_wave_config.rb
@@ -2,11 +2,11 @@
 
 CarrierWave.configure do |config|
   config.storage = :aws
+  config.aws_bucket = 'AWS_FOG_DIRECTORY' # bucket name
   config.aws_credentials = {
-    :provider               => 'AWS',
     :access_key_id      => 'AWS_ACCESS_KEY_ID',
     :secret_access_key  => 'AWS_SECRET_ACCESS_KEY'
+    :region => 'AWS_REGION',
   }
-  config.aws_bucket = 'AWS_FOG_DIRECTORY' # bucket name
 end
 
