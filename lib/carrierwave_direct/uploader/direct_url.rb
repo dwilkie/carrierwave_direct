ff7c7bbc2e2c54c9bf160b1c8e680a5bd7519621
MatthiasEngh <matthias@wescrimmage.com>
2017-06-13 10:32:57 -0500

    fix direct_aws_url with a temporary patch

--- lib/carrierwave_direct/uploader/direct_url.rb
+++ lib/carrierwave_direct/uploader/direct_url.rb
@@ -6,7 +6,7 @@ module CarrierWaveDirect
         if options[:with_path]
           url
         else
-          CarrierWave::Storage::AWSFile.new(self, CarrierWave::Storage::AWS.new(self).connection, nil).public_url
+          CarrierWave::Storage::AWSFile.new(self, CarrierWave::Storage::AWS.new(self).connection, "").public_url
         end
       end
 
