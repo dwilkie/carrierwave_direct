766c5dadf05832c9a5b453242337d27611caa049
Kenta Shimojima <kenta@loqlog.com>
2018-02-14 17:07:54 +0900

    Fix a bug of 403 when fog_public: false

--- lib/carrierwave_direct/uploader/direct_url.rb
+++ lib/carrierwave_direct/uploader/direct_url.rb
@@ -3,12 +3,42 @@ module CarrierWaveDirect
     module DirectUrl
 
       def direct_fog_url(options = {})
+        if fog_public
+          fog_public_url(options[:with_path])
+        else
+          fog_private_url(options[:with_path])
+        end
+      end
+
+      private
+
+      def fog_public_url(with_path)
         base_url = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), nil).public_url
-        if options[:with_path]
+        if with_path
           base_url + key
         else
           base_url
         end
+      end
+
+      def fog_private_url(with_path)
+        base_url = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), nil).public_url
+        if with_path
+          limited_url(key)
+        else
+          base_url
+        end
+      end
+
+      def limited_url(key)
+        local_file = local_directory.files.new(key: key)
+        expire_at = ::Fog::Time.now + fog_authenticated_url_expiration
+        local_file.url(expire_at)
+      end
+
+      def local_directory
+        connection = ::Fog::Storage.new(fog_credentials)
+        connection.directories.new(key: fog_directory)
       end
 
     end
